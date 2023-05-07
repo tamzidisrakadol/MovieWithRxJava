67257009ed52f6ed5166cfcb57ee16f201da2675
Tamzid Israk <tamzidisrak12@gmail.com>
2023-05-07 21:21:04 +0600

    showing movie list from api add MovieDetailActivity and add model class for movie details create api service,Build retrofit instance,init Http inspector create MovieDetailsNetworkDataSource create Repository , add ViewModel and viewModelFactory edit MovieDetailsModel add viewModel to MovieDetailActivity.kt add popular movie data model, add MovieDataSource for pagination add MoviePagedListRepository

---
+++ app/src/main/java/com/example/moviewithrxjava/repository/MovieDataSourceFactory.kt
@@ -0,0 +1,20 @@
+package com.example.moviewithrxjava.repository
+
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+import com.example.moviewithrxjava.model.MovieModel
+import com.example.moviewithrxjava.network.MovieDBInterface
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+class MovieDataSourceFactory(private val apiService:MovieDBInterface, private val compositeDisposable: CompositeDisposable):DataSource.Factory<Int,MovieModel>() {
+
+     val movieLiveDataSource = MutableLiveData<MovieDataSource>()
+
+
+    override fun create(): DataSource<Int, MovieModel> {
+
+        val movieDataSource = MovieDataSource(apiService,compositeDisposable)
+        movieLiveDataSource.postValue(movieDataSource)
+        return movieDataSource
+    }
+}
---
+++ app/src/main/java/com/example/moviewithrxjava/repository/MoviePagedListRepository.kt
@@ -0,0 +1,35 @@
+package com.example.moviewithrxjava.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.switchMap
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.example.moviewithrxjava.model.MovieModel
+import com.example.moviewithrxjava.network.MovieDBInterface
+import com.example.moviewithrxjava.utility.Constraints
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+class MoviePagedListRepository(private val apiService:MovieDBInterface) {
+
+    lateinit var moviePagedList:LiveData<PagedList<MovieModel>>
+    lateinit var movieDataSourceFactory: MovieDataSourceFactory
+
+    fun fetchLivePagedList(compositeDisposable: CompositeDisposable):LiveData<PagedList<MovieModel>>{
+
+        movieDataSourceFactory = MovieDataSourceFactory(apiService,compositeDisposable)
+
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(false)
+            .setPageSize(Constraints.POST_PER_PAGE)
+            .build()
+        moviePagedList = LivePagedListBuilder(movieDataSourceFactory,config).build()
+        return moviePagedList
+    }
+
+//    fun getNetworkState():LiveData<NetworkState>{
+//        return moviePagedList.switchMap {
+//
+//        }
+//    }
+
+}
